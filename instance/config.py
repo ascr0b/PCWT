ff31b41e7289de9fcaa946e557725d743fae7254
Your Name <you@example.com>
2020-07-22 16:18:32 +0300

    updates

---
+++ instance/config.py
@@ -0,0 +1,13 @@
+SECRET_KEY='change it'
+TOKEN='' # Telegram bot token
+CHANNEL='' # Telegram channel id
+# Binary paths
+FINDOMAIN = '' # Path for findomain
+AMASS = '' # Path for amass
+MASSCAN = ''
+NMAP = ''
+# Telgegram proxy. Only sock5 proxy is available
+PROXY_URL = '' 
+PROXY_PORT = ''
+PROXY_USER = ''
+PROXY_PASS = ''
